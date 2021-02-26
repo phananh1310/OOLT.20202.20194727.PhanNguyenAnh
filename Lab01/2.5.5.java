b580bec48c3b75da91e0d6637ab9af13c69fbe4d
phananh1310 <phananh13102001@gmail.com>
2021-02-26 20:42:23 +0700

    Create 2.5.5.java

---
+++ Lab01/2.5.5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class TwoNumber {
+	public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("Input 1st number: ");
+        double first = in.nextDouble();
+        System.out.printf("Input 2nd number: ");
+        double second = in.nextDouble();
+
+        System.out.printf("Sum: %.2f \n",first + second);
+        System.out.printf("Difference: %.2f \n", first - second);
+        System.out.printf("Product: %.2f \n", first * second);
+        System.out.printf("Quotient: %.2f \n", first/second);
+    }
+}
