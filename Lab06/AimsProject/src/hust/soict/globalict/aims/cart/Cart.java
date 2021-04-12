84e9c050864390037a992e78f23933436b7ff61f
phananh1310 <phananh13102001@gmail.com>
2021-04-12 12:41:11 +0700

    Update Cart.java
    free item

--- Lab06/AimsProject/src/hust/soict/globalict/aims/cart/Cart.java
+++ Lab06/AimsProject/src/hust/soict/globalict/aims/cart/Cart.java
@@ -120,6 +120,11 @@ public class Cart {
 			if (count==0) System.out.println("Not found");
 			else System.out.println("Found "+count+" DVD");
 		}
+	
+	public void getALuckyItem() {
+        int rand = (int)(Math.random() * (itemsOrdered.size()+1)) + 0;
+        itemsOrdered.get(rand).setCost(0);
+    }
 		
 	}
 	
