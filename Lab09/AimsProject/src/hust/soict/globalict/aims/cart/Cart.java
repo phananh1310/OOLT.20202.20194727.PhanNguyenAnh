688aac1b88cd64ab42334177436a1e97193cbb9e
phananh1310 <phananh13102001@gmail.com>
2021-05-18 10:42:12 +0700

    merge

---
+++ Lab09/AimsProject/src/hust/soict/globalict/aims/cart/Cart.java
@@ -0,0 +1,151 @@
+package hust.soict.globalict.aims.cart;
+import java.util.*;
+import hust.soict.globalict.aims.media.*;
+
+public class Cart {
+	
+	private ArrayList<Media> itemsOrdered = new ArrayList<Media>();
+	public static final int MAX_NUMBERS_ORDERED = 20;
+	public Cart() {
+		super();
+	}
+	
+	public void addMedia(Media media) {
+		if (itemsOrdered.size()<MAX_NUMBERS_ORDERED) {
+			itemsOrdered.add(media);
+		}
+		else {
+			System.out.println("Your cart is full!");
+		}
+	}
+	public void removeMedia(int id) {
+		if (itemsOrdered.size()==0) {
+			System.out.println("Your cart is empty!");
+		}
+		else {
+			for (Media item: itemsOrdered) {
+				if (item.getId()==id) {
+					itemsOrdered.remove(item);
+					return;
+				}
+			}
+			System.out.println("Can't find that item!");
+		}
+	}
+	
+	public void addMedia(Media [] List) {
+		for(Media item : List) {
+			this.addMedia(item);
+		}
+	}
+	
+	public void addMedia(Media media1,Media media2) {
+		this.addMedia(media1);
+		this.addMedia(media2);
+	}
+	
+	public Media DVD(int rm) { 
+		return itemsOrdered.get(rm); // take the rm th element of the list
+	}
+	public float totalCost() {
+		float cost = 0;
+		for(Media item : itemsOrdered) {
+			cost += item.getCost();
+		}
+		return cost;
+	}
+	
+	
+	public void displayCart() {
+		int i=1;
+		float cost = 0;
+		if (itemsOrdered.size() >0) {
+			System.out.println("**********************CART******************************************************************************************************");
+			for(Media item : itemsOrdered) {
+				cost += item.getCost();
+				System.out.print(i+". ");
+				i++;
+				item.display();
+			}
+			System.out.println("****************************************************************************************************************************");
+			System.out.println("Total Cost: "+cost+"$");
+		}
+		else {
+			System.out.println("Your cart is NULL!");
+		}
+	}
+	
+	public void sortCartbyCost() {
+		
+		Collections.sort(this.itemsOrdered, new DVDCostComparator());
+		this.displayCart();
+	}
+	public void sortCartbyTitle() {
+		
+		Collections.sort(this.itemsOrdered, new DVDTitleComparator());
+		this.displayCart();
+	}
+	public void sortCart() {
+		Collections.sort(this.itemsOrdered, new DVDComparator());
+		this.displayCart();
+	}
+	
+	// another way to sort using comparator of Media
+	public void sortCartByTitleCost() {
+		Collections.sort(this.itemsOrdered, Media.COMPARE_BY_TITLE_COST);
+		this.displayCart();
+	}
+	public void sortCartByCostTitle() {
+		Collections.sort(this.itemsOrdered, Media.COMPARE_BY_COST_TITLE);
+		this.displayCart();
+	}
+	
+	
+	public Media searchCartbyID(int ID) {
+		for(Media item : itemsOrdered) {
+			if (item.getId()==ID) return item;
+		}
+		return null;// not found
+	}
+	public void SearchIDandDisplay(int ID) {
+		Media dvd = this.searchCartbyID(ID);
+		if (dvd == null ) {
+			System.out.println("Not found!");
+		}
+		else {
+			System.out.println("DVD found:");
+			dvd.display();
+		}
+	}
+	
+	public void SearchAndDisplay_CartbyTitle(String title) {
+		int count=0;
+		System.out.println("Result:");
+			for(Media item : itemsOrdered) {
+				if (item.search(title)) {
+					item.display();
+					count++;
+				}
+					
+			}
+			if (count==0) System.out.println("Not found");
+			else System.out.println("Found "+count+" DVD");
+		}
+
+
+	
+
+	public void getALuckyItem() {
+        int rand = (int)(Math.random() * itemsOrdered.size()) + 0;
+        itemsOrdered.get(rand).setCost(0);
+    }
+	public int getQtyOrdered() {
+		return itemsOrdered.size();
+	}
+		
+	}
+	
+
+
+
+
---
+++ Lab09/AimsProject/src/hust/soict/globalict/aims/media/Media.java
@@ -0,0 +1,130 @@
+package hust.soict.globalict.aims.media;
+
+import java.time.LocalDate;
+import java.util.Comparator;
+
+public abstract class Media implements Comparable<Media> {
+	private static int nbMedia = 0;
+	private int id;
+	private String title;
+	private String category;
+	private float cost;
+	private LocalDate dateAdded; 
+	
+	public static final Comparator<Media> COMPARE_BY_TITLE_COST = new MediaComparatorByTitleCost();
+	public static final Comparator<Media> COMPARE_BY_COST_TITLE = new MediaComparatorByCostTitle();
+
+	public Media() {
+		super();
+		nbMedia += 1;
+		this.id = nbMedia;
+		this.dateAdded = LocalDate.now();
+	}
+	public Media(String title) {
+		super();
+		this.title = title;
+		nbMedia += 1;
+		this.id = nbMedia;
+		this.dateAdded = LocalDate.now();
+	}
+	
+
+	public Media(String title, String category, float cost) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+		nbMedia += 1;
+		this.id = nbMedia;
+		this.dateAdded = LocalDate.now();
+	}
+
+
+	public Media(String title, String category, float cost, LocalDate dateAdded) {
+		super();
+		this.title = title;
+		this.category = category;
+		this.cost = cost;
+		this.dateAdded = dateAdded;
+		nbMedia += 1;
+		this.id = nbMedia;
+	}
+	
+	public void displayMedia() {
+		
+	}
+
+	public static int getNbMedia() {
+		return nbMedia;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public float getCost() {
+		return cost;
+	}
+
+	public LocalDate getDateAdded() {
+		return dateAdded;
+	}
+	
+	public void display() {		
+	}
+	public boolean search(String title2) {
+		return false;
+	}
+	public int getLength() {
+		return 0;
+	}
+	public void addAuthor(String string) {		
+	}
+	public void removeAuthor(String string) {
+	}
+	public void play() {
+		
+	}
+	public String toString() {
+		return null;
+	}
+	public String playToString() {
+		return null;
+	}
+	public void setCost(float x){
+		this.cost=x;
+	}
+	public void addTrack(Track track) {
+		
+	}
+	public void setContent(String string) {
+		
+	}
+	@Override
+	public boolean equals(Object media) {
+		if (media instanceof Media) {
+		return (this.id==((Media)media).getId());
+		}
+		else return false;
+	}
+	 
+	@Override
+	public int compareTo(Media o) {
+		int r=this.getTitle().compareTo(o.getTitle());
+		if (r==0) 
+			return this.getCategory().compareTo(o.getCategory());
+		else 
+			return r;
+	}
+	
+
+
+}
